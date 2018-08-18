8e0d941af0bc122abe770fcb4fd83c227730a3ff
Mark Bates <mark@markbates.com>
2018-08-18 14:59:05 -0400

    fixed the dep helper

--- movinglater/dep/init.go
+++ movinglater/dep/init.go
@@ -1,33 +1,24 @@
 package dep
 
 import (
-	"os"
 	"os/exec"
 
 	"github.com/gobuffalo/genny"
-	"github.com/pkg/errors"
 )
 
 func Init(path string, verbose bool) (*genny.Generator, error) {
 	g := genny.New()
 	g.RunFn(func(r *genny.Runner) error {
-		pwd, err := os.Getwd()
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		defer os.Chdir(pwd)
-		if err := os.Chdir(path); err != nil {
-			return errors.WithStack(err)
-		}
-
-		if _, err := exec.LookPath("dep"); err != nil {
-			return err
-		}
-		cmd := exec.Command("dep", "init")
-		if verbose {
-			cmd.Args = append(cmd.Args, "-v")
-		}
-		return r.Exec(cmd)
+		return genny.Chdir(path, func() error {
+			if _, err := exec.LookPath("dep"); err != nil {
+				return err
+			}
+			cmd := exec.Command("dep", "init")
+			if verbose {
+				cmd.Args = append(cmd.Args, "-v")
+			}
+			return r.Exec(cmd)
+		})
 	})
 	return g, nil
 }
