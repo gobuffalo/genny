d0f1230c73cb79c5c193fe608ee92db87912ffcc
Antonio Pagano <apagano@wawand.co>
2018-09-27 16:34:10 -0500

    adding append function

---
+++ movinglater/gotools/append.go
@@ -0,0 +1,21 @@
+package gotools
+
+import (
+	"strings"
+
+	"github.com/gobuffalo/genny"
+	"github.com/pkg/errors"
+)
+
+func Append(gf genny.File, expressions ...string) (genny.File, error) {
+	pf, err := ParseFile(gf)
+	if err != nil {
+		return gf, errors.WithStack(err)
+	}
+
+	gf = pf.File
+	pf.Lines = append(pf.Lines, expressions...)
+
+	fileContent := strings.Join(pf.Lines, "\n")
+	return genny.NewFile(gf.Name(), strings.NewReader(fileContent)), nil
+}
