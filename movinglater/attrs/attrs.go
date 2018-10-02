f925e3e31f7c3bbe7828b594de9bf5fba7107b3a
Antonio Pagano <apagano@wawand.co>
2018-10-02 10:59:55 -0500

    adding multiple types from pop

--- movinglater/attrs/attrs.go
+++ movinglater/attrs/attrs.go
@@ -17,10 +17,30 @@ func (a Attr) GoType() string {
 	if a.goType != "" {
 		return a.goType
 	}
+
 	switch a.commonType {
+	case "text":
+		return "string"
 	case "timestamp", "datetime", "date", "time":
 		return "time.Time"
+	case "nulls.text":
+		return "nulls.String"
+	case "uuid":
+		return "uuid.UUID"
+	case "json", "jsonb":
+		return "slices.Map"
+	case "[]string":
+		return "slices.String"
+	case "[]int":
+		return "slices.Int"
+	case "slices.float", "[]float", "[]float32", "[]float64":
+		return "slices.Float"
+	case "decimal", "float":
+		return "float64"
+	case "[]byte", "blob":
+		return "[]byte"
 	}
+
 	return a.commonType
 }
 
