a30c85eaab6ad29fa27efdc77a5e2f46a6cf7786
Mark Bates <mark@markbates.com>
2018-10-17 11:53:25 -0400

    genny files can now be used as io.Writer as well as io.Reader

--- file_test.go
+++ file_test.go
@@ -1,6 +1,7 @@
 package genny
 
 import (
+	"io"
 	"io/ioutil"
 	"strings"
 	"testing"
@@ -18,4 +19,17 @@ func Test_NewFile(t *testing.T) {
 	b, err := ioutil.ReadAll(f)
 	r.NoError(err)
 	r.Equal(input, string(b))
+}
+
+func Test_File_Writer(t *testing.T) {
+	r := require.New(t)
+
+	input := "hi"
+	f := NewFile("foo.txt", strings.NewReader(input))
+	r.Equal(input, f.String())
+	i, err := io.Copy(f, strings.NewReader("HELLO"))
+	r.NoError(err)
+	r.Equal(int64(5), i)
+
+	r.Equal("HELLO", f.String())
 }
