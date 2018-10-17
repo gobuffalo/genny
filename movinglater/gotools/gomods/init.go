0ee1b753300e2f2eb36d95f536983bb86010ed2d
Mark Bates <mark@markbates.com>
2018-10-17 10:26:16 -0400

    fix go mod init

--- movinglater/gotools/gomods/init.go
+++ movinglater/gotools/gomods/init.go
@@ -28,7 +28,7 @@ func New(name string, path string) (*genny.Group, error) {
 }
 
 func Init(name string, path string) (*genny.Generator, error) {
-	if len(name) == 0 || name == "." {
+	if len(name) == 0 && path != "." {
 		name = path
 		c := build.Default
 		for _, s := range c.SrcDirs() {
