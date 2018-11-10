587d0aae781bbd3e80af025866fbefd62ac3405f
Mark Bates <mark@markbates.com>
2018-11-10 14:23:05 -0500

    adds a lot more tests arounds gomods.Init

--- movinglater/gotools/gomods/init_test.go
+++ movinglater/gotools/gomods/init_test.go
@@ -1,54 +1,99 @@
 package gomods
 
 import (
-	"context"
-	"io/ioutil"
+	"go/build"
+	"path/filepath"
+	"strings"
 	"testing"
 
+	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/genny"
 	"github.com/gobuffalo/genny/gentest"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_New_No_Modules(t *testing.T) {
-	om := modsOn
-	defer func() { modsOn = om }()
-	modsOn = false
+	envy.Temp(func() {
+		envy.MustSet(ENV, "off")
 
-	r := require.New(t)
+		r := require.New(t)
 
-	run := gentest.NewRunner()
-	gg, err := New("foo/bar", "")
-	r.NoError(err)
-	run.WithGroup(gg)
-	r.NoError(run.Run())
+		run := gentest.NewRunner()
+		gg, err := New("foo/bar", "")
+		r.NoError(err)
+		run.WithGroup(gg)
+		r.NoError(run.Run())
 
-	res := run.Results()
-	r.Len(res.Files, 0)
-	r.Len(res.Commands, 0)
+		res := run.Results()
+		r.Len(res.Files, 0)
+		r.Len(res.Commands, 0)
+	})
 }
 
 func Test_New_With_Modules(t *testing.T) {
-	om := modsOn
-	defer func() { modsOn = om }()
-	modsOn = true
-	r := require.New(t)
-	run := genny.DryRunner(context.Background())
+	envy.Temp(func() {
+		envy.MustSet(ENV, "on")
 
-	dir, err := ioutil.TempDir("", "")
-	r.NoError(err)
-	g, err := New("foo/bar", dir)
-	r.NoError(err)
-	g.With(run)
+		type ts struct {
+			name string
+			path string
+			mod  string
+		}
+		table := []ts{
+			{"", "", "github.com/gobuffalo/genny/movinglater/gotools/gomods"},
+			{"coke", "", "coke"},
+			{"github.com\\gobuffalo\\coke", "", "github.com/gobuffalo/coke"},
+			{"", "github.com\\gobuffalo\\coke", "github.com/gobuffalo/coke"},
+		}
 
-	r.NoError(run.Run())
-	res := run.Results()
-	r.Len(res.Files, 0)
-	r.Len(res.Commands, 2)
+		c := build.Default
 
-	c := res.Commands[0]
-	r.Equal([]string{genny.GoBin(), "mod", "init", "foo/bar"}, c.Args)
+		for _, src := range c.SrcDirs() {
+			table = append(table, ts{
+				name: "coke",
+				path: src,
+				mod:  "coke",
+			})
+			table = append(table, ts{
+				name: "coke",
+				path: strings.ToLower(src),
+				mod:  "coke",
+			})
 
-	c = res.Commands[1]
-	r.Equal([]string{genny.GoBin(), "mod", "tidy"}, c.Args)
+			table = append(table, ts{
+				name: "",
+				path: filepath.Join(src, "github.com", "gobuffalo", "coke"),
+				mod:  "github.com/gobuffalo/coke",
+			})
+			table = append(table, ts{
+				name: "",
+				path: strings.ToLower(filepath.Join(src, "github.com", "gobuffalo", "coke")),
+				mod:  "github.com/gobuffalo/coke",
+			})
+		}
+
+		for _, tt := range table {
+			t.Run(tt.name+tt.path+tt.mod, func(st *testing.T) {
+				r := require.New(st)
+
+				run := gentest.NewRunner()
+
+				gg, err := New(tt.name, tt.path)
+				r.NoError(err)
+				run.WithGroup(gg)
+
+				r.NoError(run.Run())
+				res := run.Results()
+				r.Len(res.Files, 0)
+				r.Len(res.Commands, 2)
+
+				c := res.Commands[0]
+				r.Equal([]string{genny.GoBin(), "mod", "init", tt.mod}, c.Args)
+
+				c = res.Commands[1]
+				r.Equal([]string{genny.GoBin(), "mod", "tidy"}, c.Args)
+			})
+		}
+
+	})
 }
