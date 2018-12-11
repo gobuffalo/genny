e26c8466f14d166c788e19ae99c5f077f9ea160b
Mark Bates <mark@markbates.com>
2018-12-11 11:58:20 -0500

    improved CompareFiles

--- gentest/files.go
+++ gentest/files.go
@@ -12,16 +12,17 @@ func CompareFiles(exp []string, act []genny.File) error {
 	if len(exp) != len(act) {
 		return fmt.Errorf("len(exp) != len(act) [%d != %d]", len(exp), len(act))
 	}
-	sort.Strings(exp)
-	sort.Slice(act, func(a, b int) bool {
-		return act[a].Name() < act[b].Name()
-	})
 
-	for i, f := range act {
-		e := exp[i]
-		a := f.Name()
-		if a != e {
-			return fmt.Errorf("expect %q got %q", a, e)
+	var acts []string
+	for _, f := range act {
+		acts = append(acts, f.Name())
+	}
+	sort.Strings(exp)
+	sort.Strings(acts)
+
+	for i, n := range exp {
+		if n != acts[i] {
+			return fmt.Errorf("expected %q to match %q", acts, exp)
 		}
 	}
 	return nil
