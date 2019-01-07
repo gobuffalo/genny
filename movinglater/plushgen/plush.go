b6af81ece72430f78cda1cb9d8a187a66cd89950
Mark Bates <mark@markbates.com>
2019-01-07 11:57:10 -0500

    remove the plushgen warning

--- movinglater/plushgen/plush.go
+++ movinglater/plushgen/plush.go
@@ -2,12 +2,7 @@ package plushgen
 
 import (
 	"github.com/gobuffalo/plushgen"
-	"github.com/markbates/oncer"
 )
-
-func init() {
-	oncer.Deprecate(0, "github.com/gobuffalo/genny/movinglater/plushgen", "Use github.com/gobuffalo/plushgen instead.")
-}
 
 // Transformer will plushify any file that has a ".plush" extension
 var Transformer = plushgen.Transformer
