d74325dde6a471e4b24d0653ea61704d13055a6b
Mark Bates <mark@markbates.com>
2019-11-21 16:55:09 -0500

    remove depgen

--- depgen/init.go
+++ depgen/init.go
@@ -1,28 +1,11 @@
 package depgen
 
 import (
-	"os/exec"
+	"fmt"
 
 	"github.com/gobuffalo/genny"
 )
 
 func Init(path string, verbose bool) (*genny.Generator, error) {
-	g := genny.New()
-	var args []string
-	if verbose {
-		args = append(args, "-v")
-	}
-
-	id, err := InstallDep(args...)
-	if err != nil {
-		return g, err
-	}
-	g.Merge(id)
-
-	cmd := exec.Command("dep", "init")
-	if verbose {
-		cmd.Args = append(cmd.Args, args...)
-	}
-	g.Command(cmd)
-	return g, nil
+	return nil, fmt.Errorf("not supported")
 }
--- depgen/update.go
+++ depgen/update.go
@@ -1,28 +1,11 @@
 package depgen
 
 import (
-	"os/exec"
+	"fmt"
 
 	"github.com/gobuffalo/genny"
 )
 
 func Update(verbose bool) (*genny.Generator, error) {
-	g := genny.New()
-	var args []string
-	if verbose {
-		args = append(args, "-v")
-	}
-
-	id, err := InstallDep(args...)
-	if err != nil {
-		return g, err
-	}
-	g.Merge(id)
-
-	cmd := exec.Command("dep", "ensure")
-	args = append(args, "-update")
-	if verbose {
-		cmd.Args = append(cmd.Args, args...)
-	}
-	return g, nil
+	return nil, fmt.Errorf("not supported")
 }
